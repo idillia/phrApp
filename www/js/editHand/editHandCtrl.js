98907ea74d6c94abc0205ee43bf44ffef898a814
idillia <mila.antonova@gmail.com>
2015-08-29 12:31:31 -0700

    Disply cards

--- www/js/editHand/editHandCtrl.js
+++ www/js/editHand/editHandCtrl.js
@@ -17,14 +17,48 @@ angular.module('editHand', [])
   $scope.handKeypad = function() {
     console.log("cell was clicked");
   }
+
+  $scope.cardRanksNumeric = ["10", "9", "8", "7", "6", "5", "4", "3", "2"];
+  $scope.cardRanks = ["A", "K", "Q", "J", "10", "9", "8", "7", "6", "5", "4", "3", "2"];
+  $scope.cardSuits = ['\u2660','\u2665', '\u2663', '\u2666'];
 }])
+
+.controller('DeckKeyboardCtrl', function($scope) {
+  function cardDeck() {
+    var ranks = ["A", "K", "Q", "J", "10", "9", "8", "7", "6", "5", "4", "3", "2"];
+    var suits = ['\u2660','\u2665', '\u2663', '\u2666'];
+    var rankSuits;
+    var fiftyTwo = [];
+    for (var s in suits) {
+      for( var r in ranks) {
+        fiftyTwo.push(ranks[r]+suits[s]);       
+      };       
+    }; 
+    return fiftyTwo;    
+  }
+  var deck = cardDeck();
+  var container = [];
+
+  for (var i = 0; i < 52; i++) {
+  if (i % 13 == 0) {
+    var row = [];
+  }
+
+  row.push(deck[i]);
+  
+  if (i != 0 && ((i+1) % 13) == 0) {
+      container.push(row);
+    }
+  }
+  $scope.cardDeck = container;
+})
 
 .controller('modalKeypadCtrl', function($scope, $ionicModal) {
   $ionicModal.fromTemplateUrl('js/keypads/cardsKeypad.html', {
     scope: $scope,
     animation: 'slide-in-up'
   }).then(function(modal) {
-    $scope.modal = modal;
+    $scope.deckModal.modal = modal;
   });
   $scope.openModal = function() {
     $scope.modal.show();
@@ -44,4 +78,4 @@ angular.module('editHand', [])
   $scope.$on('modal.removed', function() {
     // Execute action
   });
-});
+})
