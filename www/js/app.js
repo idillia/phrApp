431d172638d6537550d77e2b6e72d1dc83345132
idillia <mila.antonova@gmail.com>
2015-09-10 15:05:44 -0700

    Facebook auth

--- www/js/app.js
+++ www/js/app.js
@@ -1,19 +1,98 @@
 angular.module('starter', ['ionic', 'chat', 'firebase', 'editHand', 'underscore'])
 
+// for ui-router
+.run(["$rootScope", "$state", function($rootScope, $state) {
+$rootScope.$on("$stateChangeError", function(event, toState, toParams, fromState, fromParams, error) {
+  // We can catch the error thrown when the $requireAuth promise is rejected
+  // and redirect the user back to the home page
+  if (error === "AUTH_REQUIRED") {
+    $state.go("home");
+  }
+});
+}])
+
+// app.config(["$stateProvider", function ($stateProvider) {
+// $stateProvider
+//   .state("home", {
+//     // the rest is the same for ui-router and ngRoute...
+//     controller: "HomeCtrl",
+//     templateUrl: "views/home.html",
+//     resolve: {
+//       // controller will not be loaded until $waitForAuth resolves
+//       // Auth refers to our $firebaseAuth wrapper in the example above
+//       "currentAuth": ["Auth", function(Auth) {
+//         // $waitForAuth returns a promise so the resolve waits for it to complete
+//         return Auth.$waitForAuth();
+//       }]
+//     }
+//   })
+//   .state("account", {
+//     // the rest is the same for ui-router and ngRoute...
+//     controller: "AccountCtrl",
+//     templateUrl: "views/account.html",
+//     resolve: {
+//       // controller will not be loaded until $requireAuth resolves
+//       // Auth refers to our $firebaseAuth wrapper in the example above
+//       "currentAuth": ["Auth", function(Auth) {
+//         // $requireAuth returns a promise so the resolve waits for it to complete
+//         // If the promise is rejected, it will throw a $stateChangeError (see above)
+//         return Auth.$requireAuth();
+//       }]
+//     }
+//   });
+// }]);
+
+// app.controller("HomeCtrl", ["currentAuth", function(currentAuth) {
+//   // currentAuth (provided by resolve) will contain the
+//   // authenticated user or null if not logged in
+// }]);
+
+// app.controller("AccountCtrl", ["currentAuth", function(currentAuth) {
+//   // currentAuth (provided by resolve) will contain the
+//   // authenticated user or null if not logged in
+// }])
+
 .config(function($stateProvider, $urlRouterProvider) {
-  $urlRouterProvider.otherwise('/');
+  $urlRouterProvider.otherwise('/app/edithand');
 
     $stateProvider.state('app', {
       url:'/app',
       abstract:true,
-      templateUrl: "js/sideMenu/sideMenu.html"
+      templateUrl: "js/sideMenu/sideMenu.html",
+      controller: 'AuthCtrl'
     })
-    .state('editHand', {
+    .state('app.login', {
+      url: '/login',
+      views: {
+        'menuContent': {
+          templateUrl: 'js/auth/login.html',
+          controller: 'AuthCtrl'
+        }
+      }
+    })
+    .state('app.logout', {
+      url: '/logout',
+      views: {
+        'menuContent': {
+          templateUrl: '',
+          contreller: 'AuthCtrl'
+        }
+      }
+    })
+    .state('app.editHand', {
       url: '/edithand',
       views: {
-        'editHand': {
+        'menuContent': {
           templateUrl: 'js/editHand/editHand.html',
-          controller: 'editHandCtrl'
+          controller: 'editHandCtrl',
+          resolve: {
+            // controller will not be loaded until $waitForAuth resolves
+            // Auth refers to our $firebaseAuth wrapper in the example above
+            "currentAuth": ["Auth", function(Auth) {
+              // $waitForAuth returns a promise so the resolve waits for it to complete
+              return Auth.$waitForAuth();
+            }]
+          }
         }
       }
     })
@@ -27,35 +106,46 @@ angular.module('starter', ['ionic', 'chat', 'firebase', 'editHand', 'underscore'
     }
   })
 
-  .state('viewHand', {
+  .state('app.viewHand', {
     cache: false,
     url: '/viewhand',
     views: {
-      viewHand: {
+      'menuContent': {
         templateUrl: 'js/viewHand/viewHand.html',
-        controller: 'editHandCtrl'
+        controller: 'editHandCtrl',
+           resolve: {
+      // controller will not be loaded until $requireAuth resolves
+      // Auth refers to our $firebaseAuth wrapper in the example above
+      "currentAuth": ["Auth", function(Auth) {
+        // $requireAuth returns a promise so the resolve waits for it to complete
+        // If the promise is rejected, it will throw a $stateChangeError (see above)
+        return Auth.$requireAuth();
+      }]
+    }
       }
     }
   })
 
-  .state('chat', {
+  .state('app.chat', {
     url: '/chat',
     views: {
-      chat: {
+      'menuContent': {
         templateUrl: 'js/chat/chat.html',
         controller: 'chatCtrl'
       }
     }
   })
-  .state('login', {
-    url: '/login',
+
+  .state('app.profile', {
+    url: '/profile',
     views: {
-      chat: {
-        templateUrl: 'js/auth/login.html',
+      'menuContent': {
+        templateUrl: 'js/user/profile.html',
         controller: 'AuthCtrl'
       }
     }
   })
+
 
 })
 .run(function($ionicPlatform) {
@@ -74,16 +164,23 @@ angular.module('starter', ['ionic', 'chat', 'firebase', 'editHand', 'underscore'
 
 
 .controller('AuthCtrl', function($scope, Auth) {
+  Auth.$onAuth(function(authData){
+    if (authData === null) {
+    console.log("Not logged in yet");
+  } else {
+    console.log("Logged in as", authData.uid);
+  }
+  $scope.authData = authData;
+    })
   $scope.login = function() {
-    Auth.$authWithOAuthRedirect("facebook").then(function(authData) {
-      console.log(authData);
-    }).catch(function(error){
+    Auth.$authWithOAuthRedirect("facebook").catch(function(error){
       console.log(error);
     });
   }
   $scope.logout = function() {
     Auth.$unauth();
   }
+  
 })
 
 .controller('signupCtrl', function($scope) {
@@ -91,7 +188,28 @@ angular.module('starter', ['ionic', 'chat', 'firebase', 'editHand', 'underscore'
 
 .factory("Auth", function($firebaseAuth){
   var ref = new Firebase("https://phr.firebaseio.com/");
+  this.user = ref.getAuth();
+
+  var saveNewUser = function(userObj) {
+    ref.child('users').child(userObj.id).set(userObj);
+  };
+  this.isLoggedIn = function(){
+    return !!ref.getAuth();
+  };
+  this.loginWithPW = function(userObj, cb, cbOnReg){
+    ref.authWithPassword(userId, function(err, authData){
+      if (err) {
+        console.log("Error");
+      } else {
+        authData.email = userObj.email;
+        this.user = authData;
+        cb(authData);
+        cbOnReg && cbOnReg(true);
+      }
+    }.bind(this))
+  }
   return $firebaseAuth(ref);
+
 })
 
 
